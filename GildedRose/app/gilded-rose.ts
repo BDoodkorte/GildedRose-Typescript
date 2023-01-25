6bf166acbc5b91918752bdb075b4e95ddc934268
Babs Doodkorte <babsdoodkorte@gmail.com>
2023-01-25 14:12:48 +0000

    Refactoring - combined if statements regarding quality

--- GildedRose/app/gilded-rose.ts
+++ GildedRose/app/gilded-rose.ts
@@ -20,25 +20,20 @@ export class GildedRose {
     updateQuality() {
         for (let i = 0; i < this.items.length; i++) {
 
-            // if ( not aged bried, not backstage, not sulfuras)
             if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert' && this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
                 if (this.items[i].quality > 0) {
-                        this.items[i].quality--
+                    this.items[i].quality--
                 }
             } else {
-            // combine some of the if statements
+                // combine some of the if statements
                 if (this.items[i].quality < 50) {
                     this.items[i].quality++
                     if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].sellIn < 11) {
-                            if (this.items[i].quality < 50) {
+                        if (this.items[i].sellIn < 11 && this.items[i].quality < 50 ) {
                                 this.items[i].quality++
-                            }
                         }
-                        if (this.items[i].sellIn < 6) {
-                            if (this.items[i].quality < 50) {
+                        if (this.items[i].sellIn < 6 && this.items[i].quality < 50) {
                                 this.items[i].quality++
-                            }
                         }
                     }
                 }
