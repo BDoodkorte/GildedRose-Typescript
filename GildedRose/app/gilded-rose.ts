91e88d81c07f2c3eb0143588e3ee3cbc70f6d205
Babs Doodkorte <babsdoodkorte@gmail.com>
2023-01-25 14:26:42 +0000

    Refactoring - implemented array with names of specials to shorten if statements

--- GildedRose/app/gilded-rose.ts
+++ GildedRose/app/gilded-rose.ts
@@ -17,10 +17,14 @@ export class GildedRose {
         this.items = items;
     }
 
+    // array of special names  names = [brie, backstage, sulfuras] ---- if this != names[0] && 
+
+    public specials = ['Aged Brie' ,'Backstage passes to a TAFKAL80ETC concert','Sulfuras, Hand of Ragnaros'];
+
     updateQuality() {
         for (let i = 0; i < this.items.length; i++) {
 
-            if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert' && this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+            if (this.items[i].name != this.specials[0] && this.items[i].name != this.specials[1] && this.items[i].name != this.specials[2]) {
                 if (this.items[i].quality > 0) {
                     this.items[i].quality--
                 }
@@ -28,7 +32,7 @@ export class GildedRose {
                 // combine some of the if statements
                 if (this.items[i].quality < 50) {
                     this.items[i].quality++
-                    if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+                    if (this.items[i].name == this.specials[1]) {
                         if (this.items[i].sellIn < 11 && this.items[i].quality < 50 ) {
                                 this.items[i].quality++
                         }
@@ -38,14 +42,14 @@ export class GildedRose {
                     }
                 }
             }
-            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+            if (this.items[i].name != this.specials[2]) {
                 this.items[i].sellIn--;
             }
             if (this.items[i].sellIn < 0) {
-                if (this.items[i].name != 'Aged Brie') {
-                    if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+                if (this.items[i].name != this.specials[0]) {
+                    if (this.items[i].name != this.specials[1]) {
                         if (this.items[i].quality > 0) {
-                            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+                            if (this.items[i].name != this.specials[2]) {
                                 this.items[i].quality--
                             }
                         }
