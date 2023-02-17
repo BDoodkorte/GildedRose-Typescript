e1f762820a21c3808a687a4ed83fd66cf441a5a6
Babs Doodkorte <babsdoodkorte@gmail.com>
2023-02-17 14:18:58 +0000

    Removed uneccesary folders

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -1,3 +1,5 @@
+import { sep } from "path";
+
 export class Item {
     name: string;
     sellIn: number;
@@ -10,6 +12,9 @@ export class Item {
     }
 }
 
+// array of special names
+export const specials = ['Aged Brie' ,'Backstage passes to a TAFKAL80ETC concert','Sulfuras, Hand of Ragnaros'];
+
 export class GildedRose {
     items: Array<Item>;
 
@@ -17,53 +22,57 @@ export class GildedRose {
         this.items = items;
     }
 
+// Suggestions for refactoring:
+// 1. Write a function for each special and normal item and import them here 
+// 2. use a case break system to implement the function
+
+
     updateQuality() {
         for (let i = 0; i < this.items.length; i++) {
-            if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+
+            if (this.items[i].name != specials[0] && this.items[i].name != specials[1] && this.items[i].name != specials[2] && /Conjured.+/.test(this.items[i].name) === false) {
                 if (this.items[i].quality > 0) {
-                    if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                        this.items[i].quality = this.items[i].quality - 1
-                    }
+                    this.items[i].quality--
                 }
-            } else {
+            } else if (/Conjured.+/.test(this.items[i].name) === true){
+                this.items[i].quality -=2
+            }
+            else {
                 if (this.items[i].quality < 50) {
-                    this.items[i].quality = this.items[i].quality + 1
-                    if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].sellIn < 11) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
+                    this.items[i].quality++
+                    if (this.items[i].name == specials[1]) {
+                        if (this.items[i].sellIn < 11 && this.items[i].quality < 50 ) {
+                                this.items[i].quality++
                         }
-                        if (this.items[i].sellIn < 6) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
+                        if (this.items[i].sellIn < 6 && this.items[i].quality < 50) {
+                                this.items[i].quality++
                         }
                     }
                 }
             }
-            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].sellIn = this.items[i].sellIn - 1;
+            if (this.items[i].name != specials[2]) {
+                this.items[i].sellIn--;
             }
             if (this.items[i].sellIn < 0) {
-                if (this.items[i].name != 'Aged Brie') {
-                    if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+                if (this.items[i].name != specials[0]) {
+                    if (this.items[i].name != specials[1]) {
                         if (this.items[i].quality > 0) {
-                            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                                this.items[i].quality = this.items[i].quality - 1
+                            if (this.items[i].name != specials[2] && /Conjured.+/.test(this.items[i].name) === false) {
+                                this.items[i].quality--
+                            } else if (/Conjured.+/.test(this.items[i].name) === true){
+                                this.items[i].quality -= 2
                             }
                         }
                     } else {
-                        this.items[i].quality = this.items[i].quality - this.items[i].quality
+                        this.items[i].quality = 0
                     }
                 } else {
                     if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1
+                        this.items[i].quality++
                     }
                 }
             }
         }
-
         return this.items;
     }
 }
